12c93e1cc31d073512c6101c975cffa1dca5b2bc
Michael Peterson <michaeljoepeterson@gmail.com>
2020-08-04 20:04:19 -0600

    initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.DATABASE_URL = process.env.DATABASE_URL;
+exports.PORT = process.env.PORT || 8080;
+exports.ADMIN_S = process.env.ADMIN_S;
+exports.ADMIN_G = process.env.ADMIN_G;
+exports.DOMAINS = process.env.DOMAINS;
